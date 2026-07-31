d8dee5fe8aa165cb0d14fe1bf79cc0a21e956dd4
itscliffordonyango <cliffordonyangog@gmail.com>
2026-08-01 02:59:37 +0300

    updated secrets

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,18 +3,15 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     APP_NAME: str = "Job Finder AI"
-
     APP_VERSION: str = "0.1.0"
 
     DEBUG: bool = True
 
     API_V1_PREFIX: str = "/api/v1"
 
-    DATABASE_URL: str = (
-        "postgresql+asyncpg://postgres:password@localhost:5432/jobfinder"
-    )
+    DATABASE_URL: str
 
-    SECRET_KEY: str = "4206866afd215ab56959eb5989d3a3112016778d5a1e0e4cbbda19e9a76c3ef5"
+    SECRET_KEY: str
 
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
